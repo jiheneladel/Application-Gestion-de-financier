cfaaf9b50038868bbe42d04b49f8ad7c8d0e16d5
Houssem Makhelbi <Houssem.Makhelbi@gmail.com>
2021-05-20 01:08:31 +0100

    Payement Etudiant

--- financier/src/main/java/tn/jihen/pfa/rest/PayementRest.java
+++ financier/src/main/java/tn/jihen/pfa/rest/PayementRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/trans/")
+@RequestMapping("/api/pay/")
 //@PreAuthorize("hasRole('ADMIN')")
 public class PayementRest {
 
